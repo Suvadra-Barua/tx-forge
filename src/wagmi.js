7d39ae5eb6e9f12ea326e21b31321956475e6845
Suvadra-Barua <suvadra@universalmachine.io>
2026-01-21 00:38:44 +0600

    chore: use walletconnect id from env

--- src/wagmi.js
+++ src/wagmi.js
@@ -18,7 +18,7 @@ import {
 
 export const config = getDefaultConfig({
   appName: 'TxForge',
-  projectId: 'YOUR_WALLETCONNECT_PROJECT_ID', // Get one at https://cloud.walletconnect.com
+  projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID || 'YOUR_WALLETCONNECT_PROJECT_ID', // Get one at https://cloud.walletconnect.com
   chains: [
     // Mainnets
     mainnet,
